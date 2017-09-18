0ac4613c0724a32daee0b0cfa30c9b77b7c290ef
xuqiang <xuqiang@wafersystems.com>
2017-09-18 14:08:45 +0800

    add react router modify project constructor

--- README.md
+++ README.md
@@ -1,25 +1,4 @@
-# Color-Ring
+# Color-Picker
     The ring for color picker by svg
-# Available Props
-|Prop|Default|Type|Description|
-|-----|------|-----|-----|
-|onChange| `null` | function | return html color, eg: #FF0000|
-|arrow   | `<polygon points="25 50 0 0 50 0 25 50"/>`| Object | svg element|
-|offset  | `0`| number | arrow's offset|
-|radius  | `180`| number | circle's radius|
-|image  | `require('./ring.png')`| Object | circle's background image|
-|className  | `''`| string | ring component class name|
-|changeBackground  | `false`| boolean | background color is changed by onChange event or not.|
-|adjustAngle  | `6`| number | correction arrow angle|
-|scale  | `1`| number | arrow's scale|
-
-# Available Methods
-|Method|Props|Return|Description|
-|-----|------|-----|-----|
-|hslToRgb|`{h, s, l}`|`{r, g, b}`|convert hst to rgb|
-|rgbToHsl|`{r, g, b}`|`{h, s, l}`|convert rgb to hsl|
-|colorPicker|`rgb: {r, g, b}`|`#000000`|get html color by rgb|
-
 # License
     MIT License
-# color-picker
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './ColorRing';
+import App from './component/ColorRing';
 import registerServiceWorker from './registerServiceWorker';
+import {
+	BrowserRouter,
+	Route
+} from 'react-router-dom';
+import ColorCircle from './html/ColorCircle';
+import SaturationCircle from './html/SaturationCircle';
 
-ReactDOM.render(<App onChange={v => console.log(v)} radius={250} square={380} scale={0.8} changeBackground/>, document.getElementById('root'));
+const app = <BrowserRouter>
+	<div>
+		<Route exact path="/" component={ColorCircle}/>
+		<Route path="/color" component={ColorCircle}/>
+		<Route path="/saturation" component={SaturationCircle}/>
+		<Route path="/test" component={() => <App onChange={v => console.log(v)} radius={250} square={380} scale={0.8} changeBackground/>}/>
+	</div>
+</BrowserRouter>;
+
+//<App onChange={v => console.log(v)} radius={250} square={380} scale={0.8} changeBackground/>
+
+ReactDOM.render((app), document.getElementById('root'));
 registerServiceWorker();
