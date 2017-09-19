3acfb44cee62c3d5e9e7c2f4115d0d457c1cc21e
xuqiang <xuqiang@wafersystems.com>
2017-09-19 17:26:34 +0800

    update brightness arrow's position

--- src/component/Saturation.js
+++ src/component/Saturation.js
@@ -113,8 +113,8 @@ const Saturation = class extends Component {
 					<circle cx={diameter / 2} cy={diameter / 2} r={radius} strokeWidth={stroke} stroke={'url(#right)'} fill="none"
 					        transform={`matrix(0,-1,1,0,0, ${diameter})`}
 					        strokeDasharray={`${radius * 3.15} ${radius * 3.15}`}/>
-					<image xlinkHref={require(_switch ? './but_on.png': './but_off.png')} width={110} height={110} x={diameter / 2 - 55}
-					       y={(diameter) / 2 - 55} onClick={() => this.setState({_switch: !_switch}, () => onSwitch && onSwitch(!_switch))}/>
+					<image xlinkHref={require(_switch ? './but_on.png': './but_off.png')} width={120} height={120} x={diameter / 2 - 60}
+					       y={(diameter) / 2 - 60} onClick={() => this.setState({_switch: !_switch}, () => onSwitch && onSwitch(!_switch))}/>
 					<g fill={'#6C6D83'} transform={`translate(${x}, ${y}) rotate(${rotate} 0 0) scale(${scale})`}
 					   onTouchStart={() => {
 						   this.setState({isMove: true})
